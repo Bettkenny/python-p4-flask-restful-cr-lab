1455ee70d2fdb489fda866d9e5e2136fc2a91fdb
YOUR NAME <YOUR@EMAIL.com>
2024-01-12 19:05:01 +0300

    complete lab

--- server/models.py
+++ server/models.py
@@ -7,3 +7,11 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    image = db.Column(db.String(255), nullable=True)
+    price = db.Column(db.Float(precision=2), nullable=False)
+
+    def __repr__(self):
+        return f"Plant(name={self.name}, image={self.image}, price={self.price}"
+    
+    db.create_all()
